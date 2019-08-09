46fc72be8e5df2bffd734d9a7fc483aff4aca1ee
Cranial490 <pranjal.paliwal490@gmail.com>
2019-08-09 17:47:20 +0530

    seperated login

--- tickerTest.py
+++ tickerTest.py
@@ -5,36 +5,42 @@ import numpy as np
 from kiteconnect import KiteConnect
 from kiteconnect import KiteTicker
 
+def tokenList(kite):
+	return kite.instruments()
 
 logging.basicConfig(level=logging.DEBUG)
 
 api_key = "lu3hm9qavt86o9uq"
-api_secret="zdym5n8fpurc2a1jhlsrurjoksktuxtz"
-request_token="imGpoAdnaC1ysZv3Zdn3Yly6dcZVEjug"
-access_token = "DIf8Ieq3waWnBVDYFpIgIDpDZklo5GGN"
+access_token = "lY2V4L02TzhQHw46wwJbfsao9Lt0on7G"
 
 kite = KiteConnect(api_key=api_key)
 
-# data = kite.generate_session(request_token, api_secret)
+# print(kite.generate_session(request_token, api_secret))
+
 kite.set_access_token(access_token)
+
 kws = KiteTicker(api_key,access_token)
 
 def on_ticks(ws, ticks):
     # Callback to receive ticks.
+    logging.debug("Strating the data stream")
     logging.debug("Ticks: {}".format(ticks))
-    print(kite.positions())
 
 def on_connect(ws, response):
     # Callback on successful connect.
     # Subscribe to a list of instrument_tokens (RELIANCE and ACC here).
-    ws.subscribe([1246721])
-
+    logging.debug("Starting the connection")
+    tokenDict = tokenList(kite)
+    print(tokenDict)
+    ws.subscribe([3821313])
     # Set RELIANCE to tick in `full` mode.
-    ws.set_mode(ws.MODE_FULL, [1246721])
+    ws.set_mode(ws.MODE_FULL, [3821313])
+    logging.debug("successfully subscribed")
 
 def on_close(ws, code, reason):
     # On connection close stop the main loop
     # Reconnection will not happen after executing `ws.stop()`
+    print("Closing the connection")
     ws.stop()
 
 # Assign the callbacks.
@@ -45,3 +51,4 @@ kws.on_close = on_close
 # Infinite loop on the main thread. Nothing after this will run.
 # You have to use the pre-defined callbacks to manage subscriptions.
 kws.connect()
+
