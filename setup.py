2ea190aefd1ad74f8e997a3fc69138badd3cba6d
PP067807 <Pranjal.Paliwal@cerner.com>
2020-04-09 22:24:21 +0530

    added setup file for token management

---
+++ setup.py
@@ -0,0 +1,40 @@
+import Retriever
+import util
+from configparser import ConfigParser
+from kiteconnect import KiteConnect
+from os import path
+import csv
+
+def setup_kite_instance(localPath, access_token):
+    configParser = util.fetch_config(localPath)
+    api_key = util.get_config(configParser, 'connection', 'api_key')
+    kite = KiteConnect(api_key=api_key)
+    access_token = access_token
+    try:
+        kite.set_access_token(access_token)
+    except Exception as e:
+        print(e, "Error while setting access token")
+    return kite
+
+
+def main():
+    configPath = '/Users/pp067807/Desktop/deleteLater/workSpace/dependencies/config.properties'
+    if path.exists('./temp') == False:
+        access_token = Retriever.getAccessToken(configPath)
+        config = ConfigParser()
+        config['temp'] = {
+           'access_token': access_token
+         }
+        util.write_config_file('./temp', 'w', config)
+    config = util.fetch_config('./temp')
+    access_token = util.get_config(config,'temp', 'access_token')
+    kite = setup_kite_instance(configPath, access_token)
+    instrument_list = kite.instruments(exchange = 'NSE')
+    fieldnames = ['instrument_token','exchange_token','tradingsymbol','name','last_price','expiry','strike','tick_size','lot_size','instrument_type','segment','exchange']
+    with open(path.join('./INSTRUMENTS.csv'), 'w') as f:
+        writer = csv.DictWriter(f,delimiter=",",fieldnames=fieldnames)
+        writer.writeheader()
+        for line in instrument_list:
+            writer.writerow(line)
+
+main()
